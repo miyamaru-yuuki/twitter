246fe6aa8a92dfd7023a87d445efb7640cc0bd55
miyamaruyuuki <ajptmjg@gmail.com>
2021-05-05 11:45:25 +0900

    1145

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,8 +40,12 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            'throttle:60,1',
+            'bindings',
         ],
     ];
 
