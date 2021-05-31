dbbd534b2095b0af10fd07d0040c1215efcee431
miyamaru-yuuki <37493522+miyamaru-yuuki@users.noreply.github.com>
2021-06-01 02:49:02 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login2');
+    return view('login');
 });
 Route::get('top', 'App\Http\Controllers\TopPageController@index');
 Route::post('top', 'App\Http\Controllers\TopPageController@index');
