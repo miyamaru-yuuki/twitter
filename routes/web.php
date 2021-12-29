4a24ccef7579db8b4ca73292f5876aef6ff72d60
miyamaruyuuki <ajptmjg@gmail.com>
2021-12-29 17:43:27 +0900

    1229

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
-Route::get('top', 'App\Http\Controllers\TopPageController@index');
+Route::get('top', 'App\Http\Controllers\TopPageController@index2');
 Route::post('top', 'App\Http\Controllers\TopPageController@index');
